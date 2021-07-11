677ee16a3fb0a47ab0e68a484b83a4ed9da40d11
webbdevv <kyle.xu4@gmail.com>
2021-07-11 14:49:38 -0700

    Added icon

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -3,16 +3,19 @@ import ReactDOM  from 'react-dom'
 
 import Root from './components/root'
 import configureStore from './store/store'
-
+import ActionCable from 'actioncable'
 
 document.addEventListener("DOMContentLoaded", () => {
     let store;
+    const sockets = {} 
+    sockets.cable = ActionCable.createConsumer('ws://localhost:3000/cable')
     if (window.currentUser) {
     const preloadedState = {
       session: { id: window.currentUser.id },
       entities: {
         users: { [window.currentUser.id]: window.currentUser }
-      }
+      },
+      sockets: sockets
     };
     store = configureStore(preloadedState);
     delete window.currentUser;
