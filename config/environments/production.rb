d07c97f6e21f83b04d82365dc0f5d4b1a8c208e9
webbdevv <kyle.xu4@gmail.com>
2021-07-15 19:55:00 -0700

    Changed request origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   config.action_cable.url = "wss://relackx.herokuapp.com/cable" 
-  config.action_cable.allowed_request_origins = ['https://relackx.herokuapp.com', /http:/\/\/relackx.herokuapp.com.*/]
+  config.action_cable.allowed_request_origins = ['https://relackx.herokuapp.com', /http:\/\/relackx.herokuapp.com.*/]
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
