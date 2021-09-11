5b2a6d23613e1b34a13936023be1f8cde8a1f088
webbdevv <kyle.xu4@gmail.com>
2021-09-11 15:08:57 -0700

    mixin

--- frontend/components/landing/splash.jsx
+++ frontend/components/landing/splash.jsx
@@ -34,17 +34,36 @@ export default function Splash(props){
                         <div className="featured">
                             <div className="featured-text">
                                 <h1>
-                                    Slack is where the future works
+                                    Lackx is where the future works
                                 </h1>
                                 <p>
-                                    Transform the way you work with one place for everyone and everything you need to get stuff done
+                                    Transform the way you work with one place for everyone and everything you need to get stuff done.
                                 </p>
                                 <button className="sign-in" onClick={() => props.demoUser({email: "lfleckness0@dagondesign.com", password: "GWvFFp"})}>Demo User</button>
                                 <button onClick={() => setIsOpen(true)} className="get-started">Get Started</button>
                             </div>
+                            <div className="featured-image">
+                                <img src="https://a.slack-edge.com/96dd309/marketing/img/homepage/self-serve-campaign/hero/img-campaign-hero.jpg" alt="slack image" />
+                            </div>
                         </div>
-                        <div className="featured-image">
-                            <img src="https://a.slack-edge.com/96dd309/marketing/img/homepage/self-serve-campaign/hero/img-campaign-hero.jpg" alt="slack image" />
+                        <div className="spacing-med"></div>
+                        <div className="featured">
+                            <div className="featured-image">
+                                <a href="https://www.youtube.com/embed/ZDs056YM4fc" target="_blank">
+                                    <img src="https://a.slack-edge.com/b7cc858/marketing/img/homepage/self-serve-campaign/inline-images/video-thumbnail.jpg" alt="" data-src="https://a.slack-edge.com/b7cc858/marketing/img/homepage/self-serve-campaign/inline-images/video-thumbnail.jpg"/>
+                                </a>
+                            </div>
+                            <div className="featured-text mini-heading">
+                                <h2>
+                                    Now is your moment to build a better tomorrow
+                                </h2>
+                                <p>
+                                    We've seen what the future can be. Now it's time to decide what it will be.
+                                </p>
+                                <a href="https://www.youtube.com/embed/ZDs056YM4fc" target="_blank">
+                                    <button className="btn-outlined">Watch Video</button>
+                                </a> 
+                            </div>
                         </div>
                     </div>
                 </section>
